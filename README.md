a1f2085cd855cd0a1527df2b1269c38d5046fcfd
unknown <Mridang@Mridang-PC.(none)>
2013-05-27 19:21:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+dashclock-whatsapp
+==================
+
+A Dashclock plugin that displays information about your new Whatsapp messages
---
+++ src/com/mridang/whatsapp/WhatsappWidget.java
@@ -0,0 +1,98 @@
+package com.mridang.whatsapp;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+
+import com.bugsense.trace.BugSenseHandler;
+import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
+import com.stericson.RootTools.RootTools;
+import com.stericson.RootTools.execution.Command;
+
+/*
+ * This class is the main class that provides the widget
+ */
+@SuppressLint("SdCardPath")
+public class WhatsappWidget extends DashClockExtension {
+
+	/*
+	 * @see com.google.android.apps.dashclock.api.DashClockExtension#onCreate()
+	 */
+	public void onCreate() {
+
+		super.onCreate();
+		Log.d("WhatsappWidget", "Created");
+		BugSenseHandler.initAndStartSession(this, "fb588bc8");
+
+	}
+
+	/*
+	 * @see
+	 * com.google.android.apps.dashclock.api.DashClockExtension#onUpdateData
+	 * (int)
+	 */
+	@Override
+	protected void onUpdateData(int arg0) {
+
+		setUpdateWhenScreenOn(true);
+
+		Log.d("WhatsappWidget", "Checking for unread Whatsapp messages");
+		final ExtensionData edtInformation = new ExtensionData();
+		edtInformation.visible(false);
+
+		Log.d("WhatsappWidget", "Checking if the device is rooted");	
+		if (RootTools.isRootAvailable()) {
+
+			Log.d("WhatsappWidget", "Checking if the contacts database exists");
+			if (RootTools.exists("/data/data/com.whatsapp/databases/wa.db")) {
+
+				Log.d("WhatsappWidget", "Reading unread messages from the databases");
+				try {
+
+					Command command = new Command(0, "cd  /data/data/com.whatsapp/databases/", "sqlite3 wa.db \"SELECT display_name FROM wa_contacts WHERE unseen_msg_count > 0;\"") {
+
+						@Override
+						public void output(int id, String strLine) {
+
+							edtInformation.expandedBody((edtInformation.expandedBody() == null ? "" : edtInformation.expandedBody() + "\n") + strLine);
+
+						}
+
+					};
+					RootTools.getShell(true).add(command).waitForFinish();
+
+					edtInformation.status(String.format(getString(R.string.status), (edtInformation.expandedBody() == null ? 0 : edtInformation.expandedBody().split("\n").length)));
+				  edtInformation.visible(edtInformation.expandedBody() != null && edtInformation.expandedBody().split("\n").length > 0);
+				  Log.d("WhatsappWidget", (edtInformation.expandedBody() == null ? 0 : edtInformation.expandedBody().split("\n").length) + " unread");		
+
+				} catch (Exception e) {
+					Log.e("WhatsappWidget", "Encountered an error", e);
+					BugSenseHandler.sendException(e);
+				}
+
+			} else {
+				Log.d("WhatsappWidget", "Contacts database doesn't exist");
+			}			
+
+		} else {
+			Log.d("WhatsappWidget", "The device is not rooted");
+		}
+
+		edtInformation.icon(R.drawable.ic_dashclock);
+		publishUpdate(edtInformation);
+		Log.d("WhatsappWidget", "Done");
+
+	}
+
+	/*
+	 * @see com.google.android.apps.dashclock.api.DashClockExtension#onDestroy()
+	 */
+	public void onDestroy() {
+
+		super.onDestroy();
+		Log.d("WhatsappWidget", "Destroyed");
+		BugSenseHandler.closeSession(this);
+
+	}
+
+}
